1b5beb462bc9f43fddba7a671516da81b077a2ec
seymanuryasa <95649471+seymanuryasa@users.noreply.github.com>
2022-04-18 17:13:37 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Şeymanur Yaşa`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: ``,
+      summary: `to-be data scientist. currently, student at Istanbul Unıversity`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
